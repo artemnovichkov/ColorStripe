b8abec4aba18c19daa391a32310589508115314e
Dorian Grolaux <dgrolaux@appannie.com>
2021-06-03 17:12:47 +0200

    Improve device list UX

--- ColorStripe/Views/Devices/DevicesView.swift
+++ ColorStripe/Views/Devices/DevicesView.swift
@@ -10,7 +10,19 @@ struct DevicesView: View {
     @StateObject private var viewModel: DevicesViewModel = .init()
     @Binding var peripheral: CBPeripheral?
     @Environment(\.presentationMode) private var presentationMode
-
+    
+    private var peripherals: [CBPeripheral] {
+        viewModel.peripherals.sorted { left, right in
+            guard let leftName = left.name else {
+                return false
+            }
+            guard let rightName = right.name else {
+                return true
+            }
+            return leftName < rightName
+        }
+    }
+    
     //MARK: - Lifecycle
     
     var body: some View {
@@ -22,24 +34,33 @@ struct DevicesView: View {
             viewModel.start()
         }
     }
-
+    
     //MARK: - Private
     
     @ViewBuilder
     private var contentView: some View {
         if viewModel.state == .poweredOn {
-            List {
-                ForEach(viewModel.peripherals, id: \.identifier) { peripheral in
-                    Text(peripheral.name ?? "Unknown")
-                        .onTapGesture {
-                            self.peripheral = peripheral
-                            viewModel.identifier = peripheral.identifier.uuidString
-                            presentationMode.wrappedValue.dismiss()
-                        }
+            List(peripherals, id: \.identifier) { peripheral in
+                HStack {
+                    if let peripheralName = peripheral.name {
+                        Text(peripheralName)
+                    } else {
+                        Text("Unknown")
+                            .opacity(0.2)
+                    }
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical)
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    self.peripheral = peripheral
+                    viewModel.identifier = peripheral.identifier.uuidString
+                    presentationMode.wrappedValue.dismiss()
                 }
             }
-        }
-        else {
+            
+        } else {
             Text("Please enable bluetooth to search devices")
         }
     }
